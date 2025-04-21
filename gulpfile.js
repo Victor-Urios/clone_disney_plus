192804e0074b9b39df12a9dfd2112da543af123e
Victor-Urios <victorurios@hotmail.com>
2025-04-21 02:04:23 -0300

    criando o hero e importanto a fonte do disney+

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin'); // usar npm i --save-dev gulp-imagemin@7.1.0
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({style: 'compressed' }))  
         .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
 
@@ -14,7 +21,7 @@ function styles() {
 //     cb();
 // }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
