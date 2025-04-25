4d1da3ee028deaa7cdefd6c95b1db1d0772632a8
Victor-Urios <victorurios@hotmail.com>
2025-04-25 01:48:10 -0300

    ajuster no mobile e usando versão 2022 com 2025 mediante as aulas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]'); // aqui pega todos os botões, para selectionar um especifico usa-se [data-tab-button=star_plus] para selecionar apenas o botão do star plus
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question');
 
+    const heroSection = document.querySelector('.hero');
+    const heightHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() { 
+        const actualPosition = window.scrollY;
+
+        if (actualPosition < heightHero) {
+            hiddenHeaderElements();
+        } else { 
+            showHeaderElements();
+        }
+    })
+
+    
+    // seção de atrações botões das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
             const targetTab = button.target.dataset.tabButton;
@@ -14,7 +30,28 @@ document.addEventListener('DOMContentLoaded', function() {
             //console.log(botao.target.dataset.tabButton);
         })
     }
+    //faq accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseAnswer);
+    }
 })
+
+function hiddenHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function openOrCloseAnswer(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
